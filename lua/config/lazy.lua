c3ff0fd742d2d0523f0b91b08a01507ca4403009
Thomas Tsai <thomas.tsai@wnc.com.tw>
2025-01-04 12:10:06 +0800

    Sync my neovim options

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,8 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
     { import = "plugins" },
   },
   defaults = {
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,6 +7,7 @@ return {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-macchiato",
+      -- colorscheme = "tokyonight-moon",
     },
   }
 }
