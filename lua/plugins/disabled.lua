f04725264d72fa9db3d5f8ff6bc3fe4e7008aa6f
Thomas Tsai <thomas.tsai@wnc.com.tw>
2025-01-04 12:10:46 +0800

    Add disabled.lua to disable the plugins I don't like.

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,8 @@
+return {
+  -- disable trouble
+  -- { "folke/trouble.nvim", enabled = false },
+  -- { "hrsh7th/nvim-cmp", enabled = false },
+  -- { "garymjr/nvim-snippets", enabled = false },
+  -- { "rafamadriz/friendly-snippets", enabled =false },
+  { "lewis6991/gitsigns.nvim", enabled =false },
+}
---
+++ lua/plugins/snippet.lua
@@ -0,0 +1,9 @@
+return {
+    -- Configure snippet
+    {
+        "garymjr/nvim-snippets",
+        opts = {
+            friendly_snippets = true,
+        },
+    }
+}
